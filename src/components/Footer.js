f45fa5f49ca351f5a6fe704ab1de2edfeadc4bb4
SaraMaenza <sara.maenza98@gmail.com>
2026-01-08 20:45:30 +0100

    Add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+function Footer () {
+    return (
+        <footer className="bg-sky-950 text-gray-300 py-5 text-center text-xs border-t border-gray-400">
+            Data from CoinGecko API <br></br>
+            Designed and Developed by Sara Maenza &copy; 2026
+        </footer>
+    );
+}
+
+export default Footer;
