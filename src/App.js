bea1fda61c0684f9f4cf069bd9eac83f2dc95a27
SaraMaenza <sara.maenza98@gmail.com>
2026-01-08 18:33:12 +0100

    Update App structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import CryptoList from "./components/CryptoList";
+
 function App() {
   return (
-    <div className="App">
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <div className="flex flex-1 bg-sky-950">
+        <Sidebar />
+        <main className="flex-1">
+          <CryptoList />
+        </main>
+      </div>
     </div>
   );
 }
