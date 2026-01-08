58fe66805c54708a3568ee0887a6b0a6e0038cb5
SaraMaenza <sara.maenza98@gmail.com>
2026-01-08 20:45:54 +0100

    Add CryptoItem component

---
+++ src/components/CryptoItem.js
@@ -0,0 +1,66 @@
+import PropTypes from "prop-types";
+
+function CryptoItem({ crypto, onSelect }) {
+  return (
+    <tr className="border-b border-gray-700 cursor-pointer" onClick={onSelect}>
+      <td className="py-2 px-4 font-medium">
+        <img
+          src={crypto.image}
+          alt={crypto.name}
+          className="inline-block w-6 h-6 mr-2"
+        />
+        {crypto.name}
+      </td>
+      <td className="py-2 px-4 text-right font-medium tracking-wider">
+        {crypto.current_price !== undefined && crypto.current_price !== null
+          ? `€${crypto.current_price.toLocaleString(undefined, { maximumFractionDigits: 2 })}`
+          : "-"}
+      </td>
+      <td
+        className={`py-2 px-4 text-right tracking-wider ${
+          crypto.price_change_percentage_24h > 0
+            ? "text-green-400"
+            : "text-red-400"
+        }`}
+      >
+        {crypto.price_change_percentage_24h !== undefined && crypto.price_change_percentage_24h !== null
+          ? (
+              <>
+                {crypto.price_change_percentage_24h > 0 ? "▲" : "▼"}{" "}
+                {Math.abs(crypto.price_change_percentage_24h).toFixed(2) + "%"}
+              </>
+            )
+          : "-"}
+      </td>
+      <td className="py-2 px-4 text-right font-medium tracking-wider">
+        {crypto.market_cap !== undefined && crypto.market_cap !== null
+          ? `€${crypto.market_cap.toLocaleString(undefined, { maximumFractionDigits: 0 })}`
+          : "-"}
+      </td>
+    </tr>
+  );
+}
+
+CryptoItem.propTypes = {
+  crypto: PropTypes.shape({
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    current_price: PropTypes.number,
+    price_change_percentage_24h: PropTypes.number,
+    market_cap: PropTypes.number,
+  }).isRequired,
+    onSelect: PropTypes.func,
+};
+
+CryptoItem.defaultProps = {
+  crypto: {
+    image: "/default.svg",
+    name: "Unknown",
+    current_price: 0,
+    price_change_percentage_24h: 0,
+    market_cap: 0,
+  },
+  onSelect: () => {},
+};
+
+export default CryptoItem;
