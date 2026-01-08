853c1d3e740efd1093db014da5b6da9bb753ef8e
SaraMaenza <sara.maenza98@gmail.com>
2026-01-08 17:17:11 +0100

    Add Sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+function Sidebar() {
+  return (
+    <aside className="bg-sky-950 text-white w-56 h-screen py-6 px-3 flex flex-col space-y-6">
+        <nav className="flex flex-col space-y-4 bg-gray-400 p-8 rounded">
+            <span>Favorite</span>
+        </nav>
+    </aside>
+  );
+}
+
+export default Sidebar;
