85594fd3ccc95cceaeaedb5fed542ab3e4e5aee6
SaraMaenza <sara.maenza98@gmail.com>
2026-01-09 16:51:55 +0100

    Update style in Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 function Footer () {
     return (
-        <footer className="bg-sky-950 text-gray-300 py-5 text-center text-xs border-t border-gray-400">
+        <footer className="bg-gray-950/95 text-gray-300 py-5 text-center text-xs border-t border-gray-600">
             Data from CoinGecko API <br></br>
             Designed and Developed by Sara Maenza &copy; 2026
         </footer>
