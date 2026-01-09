816488efb518ba2ec6ca4fb9032a1fcfa90d7864
SaraMaenza <sara.maenza98@gmail.com>
2026-01-09 14:10:31 +0100

    Update App structure with new component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import CryptoList from "./components/CryptoList";
+import CryptoDetail from "./components/CryptoDetail";
+import Footer from "./components/Footer";
 
 function App() {
+
+  const [selectedCrypto, setSelectedCrypto] = useState(null);
+  const [cryptoList, setCryptoList] = useState([]);
+
   return (
-    <div className="flex flex-col min-h-screen">
+    <>
+    <div className="flex flex-col">
       <Header />
       <div className="flex flex-1 bg-sky-950">
         <Sidebar />
         <main className="flex-1">
-          <CryptoList />
+          <CryptoList 
+            onSelectCrypto={setSelectedCrypto} 
+            onCryptoListUpdate={setCryptoList}
+          />
+          <CryptoDetail
+            crypto={selectedCrypto || cryptoList[0] || {}}
+          />
         </main>
       </div>
     </div>
+    <Footer />
+    </>
   );
 }
 
