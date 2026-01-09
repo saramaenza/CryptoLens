a208dca926f0b1a2563bd320bf5beebf912e19eb
SaraMaenza <sara.maenza98@gmail.com>
2026-01-09 18:52:38 +0100

    Add light theme in Favorites

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,37 +1,40 @@
 import { useState } from "react";
 
-function Favorites({ favorites, onSelectCrypto }) {
+function Favorites({ favorites, onSelectCrypto, darkMode }) {
   const [activeId, setActiveId] = useState(null);
 
   return (
     <div>
       <h4 className="text-md font-medium flex items-center gap-1">
-        <span className="material-symbols-outlined text-amber-400">star</span>
+        <span className={`material-symbols-outlined ${darkMode ? "text-amber-500" : "text-amber-400"}`}>star</span>
         Favorites
       </h4>
-      <hr className="border-gray-600 mt-3"></hr>
+      <hr className={`${darkMode ? "border-gray-400" : "border-gray-600"} mt-3`}></hr>
       <ul className="space-y-3 pt-5 text-md">
         {favorites.length === 0 && (
-          <li className="text-gray-400">No favorites added</li>
+            <li className={`${darkMode ? "text-gray-600" : "text-gray-400"}`}>No favorites added</li>
         )}
         {favorites.map((crypto) => (
           <li
             key={crypto.id}
-            className={`cursor-pointer flex items-center px-2 py-1 rounded transition-colors ${
-              activeId === crypto.id ? "bg-gray-700" : "hover:bg-gray-700"
-            }`}
+            className={
+                `cursor-pointer flex items-center px-2 py-1 rounded transition-colors ` +
+                (activeId === crypto.id
+                ? (darkMode ? "bg-gray-300" : "bg-gray-700")
+                : (darkMode ? "hover:bg-gray-300" : "hover:bg-gray-700"))
+            }
             onClick={() => {
-              setActiveId(crypto.id);
-              onSelectCrypto(crypto);
+                setActiveId(crypto.id);
+                onSelectCrypto(crypto);
             }}
-          >
+            >
             <img
-              src={crypto.image}
-              alt={crypto.name}
-              className="inline-block w-5 h-5 mr-1"
+                src={crypto.image}
+                alt={crypto.name}
+                className="inline-block w-5 h-5 mr-1"
             />
             <span className="pl-1">{crypto.name}</span>
-          </li>
+            </li>
         ))}
       </ul>
     </div>
