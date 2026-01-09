9603cc8464297a75045f14f28b35f989bd514ebd
SaraMaenza <sara.maenza98@gmail.com>
2026-01-09 16:51:35 +0100

    Update style structure in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,18 @@
 import SearchInput from "./SearchInput";
 import DarkModeToggle from "./DarkModeToggle";
 
-function Header() {
+function Header({ allCryptos, onSelectCrypto }) {
   return (
-    <header className="bg-sky-950 p-4 flex font-medium border-b border-gray-400">
-        {/*TODO: Logo Section */}
-        <div className="flex items-center space-x-2">
-            <span className="text-white">CryptoLens</span>
-            <span className="text-gray-400">Dashboard</span>
-        </div>
-        <SearchInput />
-        <div className="ml-auto">
-            <DarkModeToggle />
-        </div>
+    <header className="bg-gray-950/95 p-4 flex items-center font-medium border-b border-gray-600">
+      <div className="flex items-center space-x-2">
+        <img className="w-9 h-9" src="https://img.icons8.com/avantgarde/100/bank.png" alt="CryptoLens" />
+        <span className="text-white">CryptoLens</span>
+        <span className="text-gray-400">Dashboard</span>
+      </div>
+      <SearchInput allCryptos={allCryptos} onSelectCrypto={onSelectCrypto} />
+      <div className="ml-auto flex items-center">
+        <DarkModeToggle />
+      </div>
     </header>
   );
 }
