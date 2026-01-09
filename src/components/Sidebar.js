07bd08fd4b440d49a68250b04697700fa0cdafb1
SaraMaenza <sara.maenza98@gmail.com>
2026-01-09 16:51:09 +0100

    Fix colours style in Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,10 @@
 import Favorites from './Favorites';
 
-function Sidebar() {
+function Sidebar({ favorites, onSelectCrypto }) {
   return (
-    <aside className="bg-sky-950 text-white w-50 py-6 px-3 flex flex-col space-y-6">
-        <nav className="flex flex-col space-y-4 p-8 rounded border border-gray-400 h-full">
-            <Favorites />
+    <aside className="text-white w-64 py-6 px-4 flex flex-col space-y-6">
+        <nav className="flex flex-col bg-gray-800 space-y-4 py-8 px-5 rounded h-full">
+            <Favorites favorites={favorites} onSelectCrypto={onSelectCrypto} />
         </nav>
     </aside>
   );
