8ae8128dc590e0256510e0c082c06c78984265f6
SaraMaenza <sara.maenza98@gmail.com>
2026-01-09 18:46:49 +0100

    Add light theme in Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,10 @@
 import Favorites from './Favorites';
 
-function Sidebar({ favorites, onSelectCrypto }) {
+function Sidebar({ favorites, onSelectCrypto, darkMode }) {
   return (
     <aside className="text-white w-64 py-6 px-4 flex flex-col space-y-6">
-        <nav className="flex flex-col bg-gray-800 space-y-4 py-8 px-5 rounded h-full">
-            <Favorites favorites={favorites} onSelectCrypto={onSelectCrypto} />
+        <nav className={`flex flex-col ${darkMode ? "bg-gray-200 border-gray-400 text-gray-900" : "bg-gray-800 border-gray-600  text-white"} border space-y-4 py-8 px-5 rounded h-full`}>
+            <Favorites favorites={favorites} onSelectCrypto={onSelectCrypto} darkMode={darkMode} />
         </nav>
     </aside>
   );
