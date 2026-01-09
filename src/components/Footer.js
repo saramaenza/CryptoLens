ad0e35a94d7d07bf2c18f7f7e06892b1d8ef7f49
SaraMaenza <sara.maenza98@gmail.com>
2026-01-09 18:52:49 +0100

    Add light theme in Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
-function Footer () {
+function Footer ({ darkMode}) {
     return (
-        <footer className="bg-gray-950/95 text-gray-300 py-5 text-center text-xs border-t border-gray-600">
+        <footer className={`${darkMode ? "bg-gray-100 text-gray-900 border-gray-400" : "bg-gray-950/95 text-gray-300 border-gray-600"} py-5 text-center text-xs border-t`}>
             Data from CoinGecko API <br></br>
             Designed and Developed by Sara Maenza &copy; 2026
         </footer>
