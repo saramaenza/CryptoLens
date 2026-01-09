6af7944d602b9b5fac05b6c1abc077ce1360f619
SaraMaenza <sara.maenza98@gmail.com>
2026-01-09 18:48:21 +0100

    Add light theme in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,18 @@
 import SearchInput from "./SearchInput";
 import DarkModeToggle from "./DarkModeToggle";
 
-function Header({ allCryptos, onSelectCrypto }) {
+function Header({ allCryptos, onSelectCrypto, darkMode, setDarkMode }) {
+
   return (
-    <header className="bg-gray-950/95 p-4 flex items-center font-medium border-b border-gray-600">
+    <header className={`${!darkMode ? "bg-gray-950/95 border-gray-600" : "bg-gray-100 border-gray-400"} p-4 flex items-center font-medium border-b`}>
       <div className="flex items-center space-x-2">
         <img className="w-9 h-9" src="https://img.icons8.com/avantgarde/100/bank.png" alt="CryptoLens" />
-        <span className="text-white">CryptoLens</span>
-        <span className="text-gray-400">Dashboard</span>
+        <span className={`${!darkMode ? "text-white" : "text-gray-900"}`}>CryptoLens</span>
+        <span className={`${!darkMode ? "text-gray-400" : "text-gray-600"}`}>Dashboard</span>
       </div>
-      <SearchInput allCryptos={allCryptos} onSelectCrypto={onSelectCrypto} />
+      <SearchInput allCryptos={allCryptos} onSelectCrypto={onSelectCrypto} darkMode={darkMode} />
       <div className="ml-auto flex items-center">
-        <DarkModeToggle />
+        <DarkModeToggle darkMode={darkMode} setDarkMode={setDarkMode} />
       </div>
     </header>
   );
